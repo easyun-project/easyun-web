317641f1f998a43698738d3d5e9bf9531d8d502d
Evan <suzukaze.hazuki2020@gmail.com>
2021-12-09 00:26:13 +0900

    chore: ci shell

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo "Kill all the running PM2 actions"
-sudo pm2 kill
+pm2 kill
 
 echo "Jump to app folder"
 cd /home/ec2-user/easyun-web
@@ -16,3 +16,6 @@ npm run build
 echo "Run new PM2 action"
 cp /home/ec2-user/ecosystem.json ecosystem.json
 pm2 start ecosystem.json
+
+echo "start client"
+nohup live-server dist --port=8088
