5fed37ba1ba50aef3ddc5be640dc31e6512020e4
Evan <suzukaze.hazuki2020@gmail.com>
2021-12-18 18:10:23 +0900

    feat(app): get host change to localStorage

--- src/views/DataCenter/index.tsx
+++ src/views/DataCenter/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {CHeader} from '@/components/Logic/CHeader';
 import {CFooter} from '@/components/Logic/CFooter';
 import {CSubnet} from '@/components/Logic/CSubnet';
@@ -6,52 +6,70 @@ import {classnames} from '@@/tailwindcss-classnames';
 import CSecurityGroup from '@/components/Logic/CSecurityGrop';
 import {CButton} from '@/components/Common/CButton';
 import {Icon} from '@iconify/react';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+import {useDispatch} from "react-redux";
+import dataCenterService from "@/service/dataCenterService";
+import {DefaultDataCenter} from "@/constant/result";
 
 const DataCenter = (): JSX.Element => {
-	const navigate = useNavigate();
-	return (
-		<div>
-			<CHeader/>
-			<div className={classnames('my-3')}>
-				<Icon icon="ant-design:plus-circle-twotone" width="25" className={classnames('inline-block','mx-1')}  fr={undefined} />
-				Create New Cloud DataCenter
-			</div>
-			<div className={classnames('ml-5', 'mt-2')}>Easyun DataCenter Networking</div>
-			<div className={classnames('ml-24')}>Region:
-				<Icon className={classnames('inline-block','mx-1')} icon="emojione-v1:flag-for-japan" color="gray" width="25" height="25" fr={undefined} />
-                japan(ap-northeast-1)</div>
-			<div className={classnames('ml-5')}>
-				<span className={classnames('mr-2')}>IPv4 CIDR block:</span>
-				<input className={classnames('border')} type="text"/>
-			</div>
-			<CSubnet index={1} isPublic={true} classes={classnames('w-96', 'inline-block')}/>
-			<CSubnet index={2} isPublic={true} classes={classnames('w-96', 'inline-block')}/>
-			<br/>
-			<CSubnet index={1} isPublic={false} classes={classnames('w-96', 'inline-block')}/>
-			<CSubnet index={2} isPublic={false} classes={classnames('w-96', 'inline-block')}/>
+    const navigate = useNavigate();
+    let dispatch = useDispatch();
+
+    const getDefaultDataCenterData = async () => {
+        const result = await dataCenterService.getDefault<DefaultDataCenter>()
+        console.log(result)
+    }
+
+    useEffect(()=>{
+        getDefaultDataCenterData()
+    })
 
 
-			<div className={classnames('ml-5', 'mt-10')}>Easyun DataCenter Security Group</div>
-			<CSecurityGroup classes={classnames('mx-5', 'w-64', 'inline-block')}/>
-			<CSecurityGroup classes={classnames('mx-5', 'w-64', 'inline-block')}/>
-			<CSecurityGroup classes={classnames('mx-5', 'w-64', 'inline-block')}/>
+    return (
+        <div>
+            <CHeader/>
+            <div className={classnames('my-3')}>
+                <Icon icon="ant-design:plus-circle-twotone" width="25" className={classnames('inline-block', 'mx-1')}
+                      fr={undefined}/>
+                Create New Cloud DataCenter
+            </div>
+            <div className={classnames('ml-5', 'mt-2')}>Easyun DataCenter Networking</div>
+            <div className={classnames('ml-24')}>Region:
+                <Icon className={classnames('inline-block', 'mx-1')} icon="emojione-v1:flag-for-japan" color="gray"
+                      width="25" height="25" fr={undefined}/>
+                japan(ap-northeast-1)
+            </div>
+            <div className={classnames('ml-5')}>
+                <span className={classnames('mr-2')}>IPv4 CIDR block:</span>
+                <input className={classnames('border')} type="text"/>
+            </div>
+            <CSubnet index={1} isPublic={true} classes={classnames('w-96', 'inline-block')}/>
+            <CSubnet index={2} isPublic={true} classes={classnames('w-96', 'inline-block')}/>
+            <br/>
+            <CSubnet index={1} isPublic={false} classes={classnames('w-96', 'inline-block')}/>
+            <CSubnet index={2} isPublic={false} classes={classnames('w-96', 'inline-block')}/>
 
-			<div className={classnames('flex', 'justify-center', 'm-16')}>
-				<CButton click={() => {
-					navigate('/home');
-				}} classes={classnames('bg-gray-400', 'text-white', 'rounded-2xl', 'w-32', 'h-9', 'mx-5')}>
-					<Icon className={classnames('inline-block', 'mr-2')} icon="akar-icons:arrow-left" color="white"
-						width="20" height="20" fr={undefined}/>
+
+            <div className={classnames('ml-5', 'mt-10')}>Easyun DataCenter Security Group</div>
+            <CSecurityGroup classes={classnames('mx-5', 'w-64', 'inline-block')}/>
+            <CSecurityGroup classes={classnames('mx-5', 'w-64', 'inline-block')}/>
+            <CSecurityGroup classes={classnames('mx-5', 'w-64', 'inline-block')}/>
+
+            <div className={classnames('flex', 'justify-center', 'm-16')}>
+                <CButton click={() => {
+                    navigate('/home');
+                }} classes={classnames('bg-gray-400', 'text-white', 'rounded-2xl', 'w-32', 'h-9', 'mx-5')}>
+                    <Icon className={classnames('inline-block', 'mr-2')} icon="akar-icons:arrow-left" color="white"
+                          width="20" height="20" fr={undefined}/>
                     Back</CButton>
-				<CButton click={() => {
-					navigate('/resource');
-				}} classes={classnames('bg-yellow-550', 'text-white', 'rounded-2xl', 'w-32', 'px-5')}>Create</CButton>
-			</div>
+                <CButton click={() => {
+                    navigate('/resource');
+                }} classes={classnames('bg-yellow-550', 'text-white', 'rounded-2xl', 'w-32', 'px-5')}>Create</CButton>
+            </div>
 
-			<CFooter/>
-		</div>
-	);
+            <CFooter/>
+        </div>
+    );
 };
 
 
