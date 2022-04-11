afb7f2385b7cb46622a36bfa4ffb3aee05c7f134
liguotong <122790345@qq.com>
2022-04-11 21:13:29 +0800

    fix：紧急修复首页代码问题

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -14,7 +14,7 @@ import { Menu, Dropdown, Spin } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 
 
-export default function Home():JSX.Element {    
+export default function Home():JSX.Element {
     const navigate = useNavigate();
     // const [datacenters,changeDatacenters] = useState<'loading'|DataCenterModel[]>('loading');
     const [sortBy,changeSortBy] = useState('Name');
@@ -81,7 +81,7 @@ export default function Home():JSX.Element {
         </div>
 
         <Spin spinning={dcListLoading} tip="Loading...">{
-        dataCenterList!.length === 0 ? <Nodc/> :
+        dataCenterList?.length === 0 ? <Nodc/> :
             <div className={classnames('grid','2xl:grid-cols-3','lg:grid-cols-2','gap-4','justify-items-center','items-center','mt-4')}>
                 {dataCenterList?.sort((a,b)=>{
                     if(order === 'dcRegion'){return ['us-west-1','us-east-1'].indexOf(b.dcRegion) - ['us-west-1','us-east-1'].indexOf(a.dcRegion); }
