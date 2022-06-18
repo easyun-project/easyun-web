2aa6276d6e7d18744b2c082a7b0130bb93fabf91
liguotong <122790345@qq.com>
2022-06-18 21:46:21 +0800

    test: 测试修复代码

--- src/views/Resource/StVolume/AddVolume/index.tsx
+++ src/views/Resource/StVolume/AddVolume/index.tsx
@@ -4,12 +4,12 @@ import { useTranslation, Trans } from 'react-i18next';
 import { Card, Switch, Select } from 'antd';
 import CTags from '@/components/Logic/CTags';
 import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
-import { useNewDisk } from '@/utils/hooks';
 import volumeService from '@/service/stVolumeService';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import { LoadingOutlined } from '@ant-design/icons';
+import { useNewDisk } from '@/utils/hooks';
 
 const { Option } = Select;
 
@@ -24,15 +24,15 @@ const AddVolume = (): JSX.Element => {
     const [ azName, changeAzName ] = useState('');
     const availableZones = [ 'us-east-1a', 'us-east-1b' ];
 
-    useEffect(()=>console.log({
+    useEffect(() => console.log({
         ...newDiskProps,
         multiattch,
     }), [ newDiskProps ]);
     return (
         <>
-            <div className= 'my-8 mx-5 text-3xl '>
+            <div className="my-8 mx-5 text-3xl ">
                 <Icon
-                    className= 'inline-block mx-5 '
+                    className="inline-block mx-5 "
                     icon="fluent:add-circle-20-regular"
                     width="50"
                     height="50"
@@ -40,43 +40,47 @@ const AddVolume = (): JSX.Element => {
                 />
                 Add Cloud Storage(S3 Bucket)
             </div>
-            <Card title={t('addVolume.zone.title')} className='mt-5 rounded-border'>
+            <Card title={t('addVolume.zone.title')} className="mt-5 rounded-border">
                 <div>{t('addVolume.zone.tip')}</div>
-                <Select defaultValue={availableZones[0]} size='small' style={{ width: 120 }} onChange={(e)=>changeAzName(e)}>
-                    {availableZones.map((zone)=><Option key ={zone} value={zone}>{zone}</Option>)}
+                <Select defaultValue={availableZones[0]} size="small" style={{ width: 120 }}
+                    onChange={(e) => changeAzName(e)}>
+                    {availableZones.map((zone) => <Option key={zone} value={zone}>{zone}</Option>)}
                 </Select>
             </Card>
 
-            <Card title={t('addVolume.configuration.title')} className='mt-5 rounded-border'>
+            <Card title={t('addVolume.configuration.title')} className="mt-5 rounded-border">
                 {newDisk}
-                <div className='flex'>
+                <div className="flex">
                     <Switch
-                        className='mt-2'
+                        className="mt-2"
                         checkedChildren={<CheckOutlined/>}
                         unCheckedChildren={<CloseOutlined/>}
-                        defaultChecked = {false}
-                        onChange = {()=>
+                        defaultChecked={false}
+                        onChange={() =>
                             changeMultiattch(!multiattch)}
                     />
-                    <div className='ml-4'>
-                        <div className='text-lg font-bold'>{ multiattch
+                    <div className="ml-4">
+                        <div className="text-lg font-bold">{multiattch
                             ? t('addVolume.configuration.multiattachConfiguration.abledTitle')
                             : t('addVolume.configuration.multiattachConfiguration.disabledTitle')}</div>
 
                         <Trans i18nKey={'addVolume.configuration.multiattachConfiguration.disabledTip'}
-                            components={{ bold:<strong/>, a:<a href="http://baidu.com" target='_blank' rel="noreferrer" className='text-blue-500 underline'/> }} />
+                            components={{ bold: <strong/>,
+                                a: <a href="http://baidu.com" target="_blank" rel="noreferrer"
+                                    className="text-blue-500 underline"/>
+                            }}/>
                     </div>
                 </div>
 
 
             </Card>
 
-            <Card title={t('addVolume.tags.title')} className='mt-5 rounded-border'>
-                <CTags tags={tags} changeTags={changeTags} />
+            <Card title={t('addVolume.tags.title')} className="mt-5 rounded-border">
+                <CTags tags={tags} changeTags={changeTags}/>
             </Card>
 
-            <button className='m-5 w-32 btn-gray' onClick={()=>navigate(-1)}>Back</button>
-            <button className='m-5 w-32 btn-yellow' onClick={()=>{
+            <button className="m-5 w-32 btn-gray" onClick={() => navigate(-1)}>Back</button>
+            <button className="m-5 w-32 btn-yellow" onClick={() => {
                 changeCreating(true);
                 // TODO:跟后端沟通接口后修改
                 volumeService.addVolume({
@@ -84,19 +88,22 @@ const AddVolume = (): JSX.Element => {
                     // multiattch,
                     azName,
                     dcName,
-                    svrId:'1234',
-                    tagName:'1234'
+                    svrId: '1234',
+                    tagName: '1234'
                 }).then(
                     () => {
                         changeCreating(false);
                         alert('创建成功');
                         navigate('/resource/volume');
                     },
-                    () => {changeCreating(false);
-                        alert('创建失败');},
+                    () => {
+                        changeCreating(false);
+                        alert('创建失败');
+                    },
                 );
             }
-            }> {creating ? <LoadingOutlined className='align-middle' /> : undefined} Create</button>
+            }> {creating ? <LoadingOutlined className="align-middle"/> : undefined} Create
+            </button>
         </>
     );
 };
