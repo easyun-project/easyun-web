6aebb776a3c881c621a0429a2a9e74321c4ae7c0
熊熊熊熊熊 <985049549@qq.com>
2022-07-17 17:12:53 +0800

    修改了生成子文件夹的逻辑

--- src/views/Resource/StBucket/BucketManage/Objects.tsx
+++ src/views/Resource/StBucket/BucketManage/Objects.tsx
@@ -8,6 +8,13 @@ import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import { StBucketDetailModel } from '@/constant/storage';
 
+type objectData = {
+    key:string,
+    name:string,
+    size:number,
+    modefied:string,
+    type:string }
+
 
 export default function Objects() {
     const dcName = useSelector((state: RootState) => state.dataCenter.currentDC.basicInfo!.dcName);
@@ -15,11 +22,15 @@ export default function Objects() {
     const { bucketId } = currentBucket.bucketBasic;
     const { isLoading, isError, data, error } = useQuery([ bucketId, dcName ], ()=>bucketService.getBucketObjects({ bucketId, dcName }));
     const [ path, setPath ] = useState('');
-    const dataSource = data?.filter(item=>  {
-        const isSon = item.key.startsWith(path);
-        const isSonFile = item.key.replace(path, '').split('/').length === 1 && item.key.replaceAll(path, '') !== '';
-        const isSonFolder = item.key.replace(path, '').split('/').length === 2 && item.key.replaceAll(path, '').split('/').at(-1) === '';
-        return isSon && (isSonFile || isSonFolder);
+
+    //子文件的数据源
+    const sonFilesData = data?.filter(item =>  {
+        const isOffspring = item.key.startsWith(path);
+        const isFolder = item.key.endsWith('/');
+        const isFile = !isFolder;
+        const isSonFile = isOffspring && isFile && item.key.replace(path, '').indexOf('/') === -1;
+        // const isSonFolder = isOffspring && item.key.replace(path, '').split('/').length === 2 && item.key.replaceAll(path, '').split('/').at(-1) === '';
+        return isSonFile;
     }).map(item => {
         return {
             key:item.key,
@@ -28,26 +39,62 @@ export default function Objects() {
             modefied:item.modifiedTime,
             type:item.type };
     });
-    //     [
-    //     {
-    //         key: '1',
-    //         name: 'hello.txt',
-    //         size: '12Mb',
-    //         modified: '2022',
-    //     },
-    //     {
-    //         key: '2',
-    //         name: 'hello文件夹',
-    //         size: '',
-    //         modified: '2021',
-    //     },
-    // ];
+    //子文件夹的数据源：1.需要找到返回的子文件夹列表。2.需要从孙文件的路径中解析出子文件夹，并去重。
+    //立即执行函数写法，用Set去重
+    const sonFoldersData = ((data) =>{
+        const sonFoldersList:objectData[] = [];
+        const sonFolders = new Set();
+        //处理子文件夹
+        data?.filter(item =>  {
+            const isOffspring = item.key.startsWith(path);
+            const isSonFolder = isOffspring && item.key.replace(path, '').split('/').length === 2 && item.key.replaceAll(path, '').split('/').at(-1) === '';
+            return isSonFolder;
+        }).forEach(item => {
+            //生成data对象前将路径加入到set中
+            sonFolders.add(item.key.replace(path, ''));
+            sonFoldersList.push({
+                key:item.key,
+                name:item.key.replace(path, ''),
+                size:item.size,
+                modefied:item.modifiedTime,
+                type:item.type });
+        });
+        //处理孙文件
+        data?.filter(item =>  {
+            const isOffspring = item.key.startsWith(path);
+            const isFile = !item.key.endsWith('/');
+            const isGrandSonFile = isOffspring && isFile && item.key.replace(path, '').indexOf('/') !== -1;
+            return isGrandSonFile;
+        }).forEach(item => {
+            //生成data对象前将路径加入到set中
+            const folderPath = item.key.replace(path, '').split('/')[0] + '/';
+            if(!sonFolders.has(folderPath)){
+                sonFolders.add(folderPath);
+                sonFoldersList.push({
+                    key:item.key,
+                    name:folderPath,
+                    size:0,
+                    modefied:item.modifiedTime,
+                    type:'Folder' });
+            }
+        });
+        return sonFoldersList;
+    })(data);
+    //合并子文件数据源和子文件夹数据源
+    const dataSource = [ ...sonFoldersData, ...sonFilesData ? sonFilesData : []  ];
+
     const columns = [
         {
             title: 'Name',
             dataIndex: 'name',
             key: 'name',
-            onclick:(item)=>console.log(item)
+            render: (text: string, record) => {
+                if(record['type'] === 'Folder'){
+                    return <a className='text-blue-500' onClick={()=>setPath(path + text)}>{text}</a>;
+                }else{
+                    return <span>{text}</span>;
+                }
+            },
             // TODO:筛选功能
             // filters: [
             //     {
@@ -84,11 +131,12 @@ export default function Objects() {
             <div>
                 <Breadcrumb>
                     <Breadcrumb.Item>
-                        <HomeOutlined />
+                        <HomeOutlined onClick={()=>setPath('')}/>
                     </Breadcrumb.Item>
-                    {path.split('/').map(item=><Breadcrumb.Item key={item}>
-                        {item}
-                    </Breadcrumb.Item>)}
+                    {path.split('/').map(item=>
+                        <Breadcrumb.Item key={item}>
+                            {item}
+                        </Breadcrumb.Item>)}
 
                 </Breadcrumb>
             </div>
