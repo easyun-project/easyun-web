fa8f80a34b4c108da669eb166c02ddb9c147e69b
Aleck <me@myners.net>
2026-04-05 12:47:17 +0800

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
@@ -7,7 +8,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, path.resolve(__dirname, 'envs'));
 
     return {
-        plugins: [ react() ],
+        plugins: [ react(), tailwindcss() ],
         envDir: 'envs',
         base: env.VITE_BASE_URL || './',
         resolve: {
